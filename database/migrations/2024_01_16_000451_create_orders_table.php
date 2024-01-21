a3818601e2e9fe4723a9d761e2783cd6adf940a2
fausto <faustodiazarboled@gmail.com>
2024-01-21 16:24:53 -0500

    creando migraciones proyecto yeli-store-api

--- database/migrations/2024_01_16_000451_create_orders_table.php
+++ database/migrations/2024_01_16_000451_create_orders_table.php
@@ -11,14 +11,15 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('orders', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('type', 100);
+            $table->float('amount', 100);
+            $table->char('status', 3);
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
@@ -27,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('orders');
     }
 };
