abfa7e5fb616bb6efb49a0d48b54e938e721d938
fausto <faustodiazarboled@gmail.com>
2024-01-22 21:36:50 -0500

    creando rutas de yeli-store-api

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,12 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\RoleController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
+use App\Models\Role;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +19,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::resource('/user', UserController::class);
+Route::resource('/role', RoleController::class);
+Route::resource('/product', ProductController::class);
+Route::resource('/category', CategoryController::class);
+Route::resource('/order', OrderController::class);
